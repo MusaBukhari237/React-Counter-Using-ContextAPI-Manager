8642d50abed48b6b2bd490707e42125a953d49a5
MusaBukhari237 <applemoosa123@gmail.com>
2020-07-22 04:33:37 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is <code>React</code> Box Controller.
+        </p>
+    <p>Hello Musa Bukhari</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
